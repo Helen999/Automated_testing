a7584161fe9575ec654dd69705e6e129eb0f4a2e
Alyona Levina <levina.av@gmail.com>
2021-10-05 15:09:54 +0600

    My first task. Part 1, task 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//В массиве целых чисел поменять местами максимальный отрицательный элемент и минимальный положительный.
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rndArray = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rndArray.Next(-10, 10);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            // First task
+
+            int a = array.Min();
+            int b = array.Max();
+            for (int k = 0; k < array.Length - 1; k++)
+            {
+                if (array[k] < a && array[k] < 0)
+                {
+                    a = array[k];
+                }
+                if (array[k] > 0 && array[k] < b)
+                { b = array[k]; }
+            }
+            for (int k = 0; k < array.Length; k++)
+            {
+                if (array[k] == a) { array[k] = b; }
+                else if (array[k] == b) { array[k] = a; }
+
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
