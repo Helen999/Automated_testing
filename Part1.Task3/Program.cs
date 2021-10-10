9f44aba93f65f8bc8fb036a135d57bcab2ae54b1
Alyona Levina <levina.av@gmail.com>
2021-10-10 23:21:12 +0600

    Part1.Task3

---
+++ Part1.Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Part1.Task3
+{
+    class Program
+    {
+        //В массиве целых чисел заменить нулями отрицательные элементы.
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rndArray = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rndArray.Next(-10, 10);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0) 
+                { 
+                    array[i] = 0; 
+                }
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
