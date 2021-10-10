846702b2147579735d3035f3702b540f74800f75
Alyona Levina <levina.av@gmail.com>
2021-10-10 23:10:42 +0600

    Part1.Task2

---
+++ Part1.Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Part1.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rndArray = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rndArray.Next(-10, 10);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            int sum = 0;
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine("The sum of numbers on even indexes is: " + sum);
+
+            Console.WriteLine();
+        }
+    }
+}
