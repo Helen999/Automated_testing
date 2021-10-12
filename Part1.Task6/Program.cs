b8b8e781f37e4da63eb77e95b2eed92a0bb50eaa
Alyona Levina <levina.av@gmail.com>
2021-10-12 21:29:41 +0600

    Part2.Task1

---
+++ Part1.Task6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1.Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array6 = new int[20];
+            Random rndArray6 = new Random();
+            
+            for (int i = 0; i < array6.Length; i++)
+            {
+                array6[i] = rndArray6.Next(-10, 10);
+            }
+
+            foreach (var item in array6)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            var dict = new Dictionary<int, int>();
+
+            foreach (var value in array6)
+            {
+                // When the key is not found, "count" will be initialized to 0
+                dict.TryGetValue(value, out int count);
+                dict[value] = count + 1;
+            }
+
+            foreach (var pair in dict)
+                if (pair.Value > 1)
+                {
+                   Console.WriteLine("Value {0} occurred {1} times.", pair.Key, pair.Value);
+                }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Part2.Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number of lines: ");
+
+            int numberOfLines = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[numberOfLines];
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+
+            int itemLong = 0; 
+            int itemShort= 0;
+            string longest ="";
+            string shortest = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (itemLong < array[i].Length)
+                {
+                    itemLong = array[i].Length;
+                    longest = array[i];
+                }
+            }
+
+            foreach (var item in array)
+            {
+                if (item.Length < shortest.Length) {
+                    shortest = item;
+                    itemShort = item.Length;
+                }
+            }
+                       
+            Console.WriteLine($" The longest line is: {longest} and quantity of symbols is: {itemLong}");
+            Console.WriteLine($" The shortest line is: {shortest} and quantity of symbols is: {itemShort}");
+        }
+        
+    }
+}
